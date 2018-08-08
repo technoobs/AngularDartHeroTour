81c7b0abd70564086af3192832c3c78351681b4d
billcyz <billcyz@gmail.com>
2018-08-09 08:45:37 +1200

    Added mock heroes file, and make some changes to display multiple heroes

--- lib/src/hero_tour/hero_tour_component.dart
+++ lib/src/hero_tour/hero_tour_component.dart
@@ -1,16 +1,19 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 
-import '../../hero.dart';
+import '../hero.dart';
+import '../mock_heroes.dart';
 
 @Component(
   selector: 'app-hero-tour',
   styleUrls: ['hero_tour_component.css'],
   templateUrl: 'hero_tour_component.html',
-  directives: [formDirectives],
+  directives: [coreDirectives, formDirectives],
 )
 
 class HeroTourComponent {
   final title = "Tour of Heroes";
-  Hero hero = Hero(1, "Windstorm");
+  // Hero hero = Hero(1, "Windstorm");
+
+  List<Hero> heroList = mockHeroes;
 }
---
+++ lib/src/mock_heroes.dart
@@ -0,0 +1,19 @@
+import 'hero.dart';
+
+final mockHeroes = <Hero>[
+  Hero(6, 'Hero Number 6'),
+  Hero(7, 'Hero Number 7'),
+  Hero(8, 'Hero Number 8'),
+  Hero(9, 'Hero Number 9'),
+  Hero(10, 'Hero Number 10'),
+  Hero(11, 'Mr. Nice'),
+  Hero(12, 'Narco'),
+  Hero(13, 'Bombasto'),
+  Hero(14, 'Celeritas'),
+  Hero(15, 'Magneta'),
+  Hero(16, 'RubberMan'),
+  Hero(17, 'Dynama'),
+  Hero(18, 'Dr IQ'),
+  Hero(19, 'Magma'),
+  Hero(20, 'Tornado')
+];
